eb9468b2fa62e2438fbaef96207b63e85b59aad1
prem-developments <premchandmalhi969@gmail.com>
2024-12-12 14:04:30 +0200

    first commit

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -5,15 +5,15 @@ function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="nav-bar">
-      {/* <img className="logo" src={"/icons/logo.svg"} alt="Logo" /> */}
       <h1 className="logo">Prem</h1>
       <div className={`nav-items ${isOpen ? "show" : ""}`}>
         <a href="#about"><p>About</p></a>
         <a href="#projects"><p>Projects</p></a>
-        <a href="#contact"><button className="primary-button">Contact Me</button></a>
+        <a href="#contact"><button className="primary-button-2">Contact Me</button></a>
       </div>
       <div className="menu-contact-container">
-       <a href="#contact"><button  className="primary-button">Contact Me</button></a>
+       <div className="primary-button-1">
+        <a href="#contact"><button  className="primary-button-2">Contact Me</button></a></div>
         <div
           className={`hamburger ${isOpen ? "open" : ""}`}
           onClick={() => setIsOpen(!isOpen)}
